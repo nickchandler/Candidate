289f89d7135c069cbbaf7521213ba63cec7232d0
nick.chand.20@gmail.com <Nick@Nicholass-MacBook-Pro.local>
2019-11-16 17:01:53 -0800

    Add endpoint for fetching user's member list from db

--- server/index.js
+++ server/index.js
@@ -5,3 +5,7 @@ let app = express();
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+
+app.get('/mymembers', (req, res) => {
+  //call a db query defined in db.models.js for all members saved by the user
+});
