efce3f99bbd0e991e688a28687795ecaea6ccca9
nick.chand.20@gmail.com <Nick@Nicholass-MacBook-Pro.local>
2019-11-18 11:37:48 -0800

    Implement structure for MemberList and MemberCard components

--- components/MemberCard.js
+++ components/MemberCard.js
@@ -1,1 +1,25 @@
 import React from 'react';
+import {View, Text, Image, Stylesheet} from 'react-native';
+
+let MemberCard = function(props) {
+  return (
+    <View>
+      <Text style={styles.cardHeader}>
+        {props.member.short_title} {props.member.first_name}{' '}
+        {props.member.last_name}, {props.member.party}-{props.member.state}
+      </Text>
+      <Image
+        // eslint-disable-next-line prettier/prettier
+        src={`https://theunitedstates.io/images/congress/original/${props.member.member_id}.jpg`}
+        style={styles.memberImage}
+      />
+    </View>
+  );
+};
+
+const styles = Stylesheet.create({
+  cardHeader: {},
+  memberImage: {},
+});
+
+export default MemberCard;
--- components/MemberList.js
+++ components/MemberList.js
@@ -1,1 +1,19 @@
 import React from 'react';
+import {Stylesheet, FlatList, View} from 'react-native';
+import MemberCard from './MemberCard';
+
+let MemberList = function(props) {
+  return (
+    <FlatList
+      data={props.members}
+      renderItem={({member}) => {
+        <MemberCard member={member} />;
+      }}
+      keyExtractor={member => member.member_id}
+    />
+  );
+};
+
+const styles = Stylesheet.create({});
+
+export default MemberList;
