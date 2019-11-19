3144b3f1d622bfaf38745456c7b25f9db97108ff
nick.chand.20@gmail.com <Nick@Nicholass-MacBook-Pro.local>
2019-11-18 20:35:11 -0800

    Implement connection to mongodb and build member schema, model

--- db/models.js
+++ db/models.js
@@ -1,1 +1,21 @@
 const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/mvp', {useNewUrlParser: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.on('open', () => {
+  console.log('connected to mongoDB');
+});
+
+var memberSchema = new mongoose.Schema({
+  id: String,
+  short_title: String,
+  first_name: String,
+  last_name: String,
+  party: String,
+  state: String,
+});
+
+var Member = mongoose.model('Member', memberSchema);
+
+module.exports = Member;
